95ece11baef80fc02ef939cab21bef4503718fbb
5rshv <serishev.alesha@gmail.com>
2024-11-14 07:19:43 +0300

    HW7_p1

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        String firstName = " Ivanov";
-        String middleName = " Ivan";
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
         String lastName = " Ivanovich";
         String fullName = "Ivanov I. I.";
-        String zamenaBukvi = "ПёС";
-        System.out.println(" Ф. И. О. сотрудника — "+  firstName + middleName+ lastName );
+        String zamenaBukvi = "Иванов Семён Семёнович";
+        System.out.println(" Ф. И. О. сотрудника — "+  firstName + " " + middleName+ " " + lastName );
         System.out.println(" Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase() );
         System.out.println(zamenaBukvi.replace("Ё", "Е").replace("ё", "е"));
     }
