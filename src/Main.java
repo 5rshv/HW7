a6fe6c64b80b86d28752f0394a415bde2c2ae039
5rshv <serishev.alesha@gmail.com>
2024-11-14 07:26:23 +0300

    HW7_p1

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ public class Main {
         String lastName = " Ivanovich";
         String fullName = "Ivanov I. I.";
         String zamenaBukvi = "Иванов Семён Семёнович";
-        System.out.println(" Ф. И. О. сотрудника — "+  firstName + " " + middleName+ " " + lastName );
-        System.out.println(" Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase() );
+        System.out.println("Ф. И. О. сотрудника — "+  firstName + " " + middleName+ " " + lastName );
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase() );
         System.out.println(zamenaBukvi.replace("Ё", "Е").replace("ё", "е"));
     }
 }
